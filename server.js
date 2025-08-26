29a4c5cb89a4a4e6320e96af1e08ff785438cd8e
Hamza Sajjad <askhamzasajjad@gmail.com>
2025-08-26 14:04:59 +0500

    change

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.use('/api/v1', routes);
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📚 Swagger UI: http://localhost:${PORT}/api-docs`);
 });
