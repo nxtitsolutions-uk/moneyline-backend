5fcdda0cd2b0dabad3985e7a06961fc36cbea890
Hamza Sajjad <askhamzasajjad@gmail.com>
2025-10-02 10:17:26 +0500

    sportsradar integration removed

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ const s3Routes = require('./s3Route');
 const invitationRoutes = require('./invitationRoute');
 const profileRoutes = require("./profileRoute");
 const userRoutes = require('./userRoute');
-const cricketRoutes = require('./cricketRoute');
 const sportRoutes = require('./sportRoute');
 const subscriptionRoutes = require('./subscriptionRoute');
 const notificationRoutes = require('./notificationRoute');
@@ -42,7 +41,6 @@ router.use('/s3', authenticate,s3Routes);
 router.use('/invitation',authenticate, invitationRoutes)
 router.use("/profile", profileRoutes);
 router.use('/user', authenticate, userRoutes);
-router.use('/cricket', authenticate, cricketRoutes);
 router.use('/sports', sportRoutes);
 router.use('/subscription', authenticate, subscriptionRoutes);
 router.use('/notification', authenticate, notificationRoutes);
